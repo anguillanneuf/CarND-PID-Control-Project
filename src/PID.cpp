8995469f539cd87bb07fa39e3e8fe8bd1d4a9931
anguillanneuf <tianzih@outlook.com>
2017-06-04 10:36:33 -0400

    50mph okay

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,32 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this -> Kp = Kp;
+    this -> Ki = Ki;
+    this -> Kd = Kd;
+
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
 
 double PID::TotalError() {
+    double steer = -Kp * p_error - Ki * i_error - Kd * d_error;
+
+    if (steer > 1.0){
+        steer = 1.0;
+    }
+
+    if (steer < -1.0){
+        steer = -1.0;
+    }
+
+    return steer;
 }
 
